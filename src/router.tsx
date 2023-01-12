2c33f9cf70a8f0e6c2ed86ba1d194582f3bf0aef
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2023-01-11 22:00:43 -0300

    feat Router

---
+++ src/router.tsx
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+/*
+Ele será responsável por renderizar as rotas da nossa aplicação, 
+de acordo com os alias que configuramos em RoutePATH:
+*/
+
+const Router = () => {
+
+    return 
+
+    
+};
+
+export default Router;
