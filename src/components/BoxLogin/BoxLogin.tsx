d4c3a174e4e4fe9d913c2bcf814ca1f5b785ef10
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2023-01-12 16:21:48 -0300

    feat BoxLogin

--- src/components/BoxLogin/BoxLogin.tsx
+++ src/components/BoxLogin/BoxLogin.tsx
@@ -1,7 +1,29 @@
 
+import { HTMLAttributes, useState } from 'react';
+import * as S from "./style";
+
+type BoxLoginType =  HTMLAttributes<HTMLDivElement>
 
 
-const BoxLogin = () => {
+export type BoxLoginProps = {
+    // essa propriedade só é assionada quando tiver informação 
+    onSubmitData: (data: {name: string, password: string}) => void
+    errorMessage: string
+  } & BoxLoginType;
+
+const BoxLogin = ({onSubmitData, errorMessage}: BoxLoginProps) => {
+
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+  // função que envia as informações digitadas/ ela é do tipo void porque não precisa retornar nada ela só vai enviar informaçao
+  const handleSubmit = (): void => {
+    // o data está recebendo de forma desconstruída {name, password}
+      const data = {name, password};
+      // mandando essas informações passando como propriedade
+      onSubmitData(data);// já passando-á para nosso backend
+  }
+
 
     return
 };
