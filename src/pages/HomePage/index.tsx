20081a5276875165a22f09315f3374f1a4998290
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2023-02-05 16:15:54 -0300

    removendo gameList

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,13 +1,11 @@
 
- import GameList from "../../components/GameList/GameList";
 import * as S from "./style";
-
 
 const HomePage = () => {
 
     return (
           <S.body>
-                <GameList></GameList>
+          
           </S.body>
     )
 };
---
+++ src/types/game.ts
@@ -0,0 +1,14 @@
+
+
+export interface GameResponse {
+
+
+    title: string,
+    coverImageUrl: string,
+    description: string,
+    year: string,
+    imdbScore: string,
+    trailerYouTubeUrl: string,
+    gameplayYouTubeUrl: string,
+
+}
