64f437a2d4ffd2ee0b850cccc94382b7295b105c
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2023-02-07 15:02:19 -0300

    fixing error

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -5,7 +5,9 @@ import { useNavigate } from "react-router-dom";
 import { RoutePath } from "types/routes";
 import * as S from "./style";
 
-
+import {GameLis} from "../../mocks/gameList"
+import GameList from "components/GameList/GameList";
+import { ProductResponse } from "types/Product";
 
 
 
@@ -15,6 +17,11 @@ const HomePage = () => {
 
     // ela recebe uma rota e nos direcionara para essa rota 
     const handleNavigation = (path: RoutePath) => navigate(path);
+
+
+    //Vamos implementar a função handleSelection para que quando o usuário clique em uma pizza, ela seja adicionada automaticamente na lista do pedido.
+const handleSelection = (product: ProductResponse) => {}
+
 
 const navigate = useNavigate();
 
@@ -28,6 +35,17 @@ const navigate = useNavigate();
     onNavigate={handleNavigation}
     onLogout={() => navigate(RoutePath.LOGIN)}
     />
+
+{Boolean(GameLis.length) &&
+ GameLis.map((product, index) => (
+    <GameList
+      product={product}
+      key={index}
+      onSelect={handleSelection}
+
+
+    />
+    ))}
           </S.body>
     )
 };
